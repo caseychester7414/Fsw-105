8bd833f73a588cf369415f2a443798130a5297f6
caseychester7414 <casey.chester@bryanuniversity.edu>
2020-02-16 13:49:51 -0800

    week 1

---
+++ week1/grocerystore.js
@@ -0,0 +1,11 @@
+var shopper = {
+    shopper ="casey",
+    Age = 24,
+    FoodColor: ["apple", "strawberry", "peach"],
+    LikesToShop: false,
+    varshopper: function(){
+
+    }    
+}
+    var fruit = ["apple", "strawberry", "peach"]
+    console.log(shopper)
