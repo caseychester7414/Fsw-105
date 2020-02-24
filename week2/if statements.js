90ce91791bc023c85250072687c5db2315c7fa23
caseychester7414 <casey.chester@bryanuniversity.edu>
2020-02-23 19:57:12 -0800

    ifstatement

--- week2/if statements.js
+++
@@ -1,9 +0,0 @@
-if(5 > 3) {
-    console.log("is greater than");
-}
-if(cat === 3) {
-    console.log(cat)
-}
-if(dog === cat) {
-    
-}
---
+++ week2/ifstatement/if_statements.js
@@ -0,0 +1,28 @@
+ if(5 > 3) {
+     console.log("is greater than");
+ }
+if(true){
+    console.log("is the length");
+ }
+  if (false);{
+ ("dog" === "cat") 
+  console.log("not the same")
+  }
+  // bronze medal
+var person = {
+    name: "Casey",
+    age: 12,
+}
+console.log(person.age)
+if(person.age >= 18 && human.name.startsWith('B')) 
+{
+    console.log("bobby is allowed to go to the movie")
+} else {
+    console.log("Bobby is not allowed to go to the movie")
+}
+if(1 === "1" || 1 == "1"){
+    console.log("strict", "loose")
+}
+else if(1 !== "1"){
+    console.log("not equal at all")
+}
