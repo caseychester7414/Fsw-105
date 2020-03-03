197e49c1ad15177975e17449caae0ff1fb39d86c
caseychester7414 <casey.chester@bryanuniversity.edu>
2020-03-03 09:01:05 -0800

    functions

--- week3/w3functions/functions.js
+++ week3/w3functions/functions.js
@@ -1,38 +1,37 @@
-// number 1
-// function addtwointers(num1, num2)
-// {
-//     return "adding" + num1 + "with" + num2 + "returns a sum of:" + (num1 +num2)
-// }
-// console.log(addTwoInters(25,15));
-// number 2
+
+function addtwointers(num1, num2)
+{
+    return "adding" + num1 + "with" + num2 + "returns a sum of:" + (num1 +num2)
+}
+console.log(addTwoInters(25,15));
+
 function addthreeinters(num1,num2,num3){
 var max =Math.max(num1,num2,num3)
 return max;
 }
 console.log(addthreeinters(21,5,30));
-// number 3 
-// function oddOrEven(i){
-//     if (i % 2 === 0)
-//     {
-//         console.log(i + "is even")
-//     } else if( i % 2 !==0)
-//         {
-//             console.log(i + "is odd")
-//         }
-//     }
-//     oddOrEven(23)
-//8 is odd
-//number 4
-// var str = "functions"
-// var longString = "functions are not so bad "
-// function string(str1){
-//    console.log(str1.length)
-//    if (str1.length <= 20){
 
-// console.log(str1.concat(str1))
-//    } else {
-//     console.log (str1.slice(0, str1.length/2))
-//    }
-// }
-// string(longString)
-// string(str)
+function oddOrEven(i){
+    if (i % 2 === 0)
+    {
+        console.log(i + "is even")
+    } else if( i % 2 !==0)
+        {
+            console.log(i + "is odd")
+        }
+    }
+    oddOrEven(22)
+
+var str = "functions"
+var longString = "functions are not so bad "
+function string(str1){
+   console.log(str1.length)
+   if (str1.length <= 20){
+
+console.log(str1.concat(str1))
+   } else {
+    console.log (str1.slice(0, str1.length/2))
+   }
+}
+string(longString)
+string(str)
