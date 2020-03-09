274b43d2d7ecfb7d29d23669f00f5c26221d5d9c
caseychester7414 <casey.chester@bryanuniversity.edu>
2020-03-08 19:53:42 -0700

    Cal

---
+++ week4/calculator/calc.js
@@ -0,0 +1,34 @@
+var readlineSync = require('readline-sync');
+
+function addTwoNumbers(num1,num2){
+    var sum = num1 + num2;
+    console.log("the result", sum)
+}
+function timesTwoNumbers(num1,num2){
+    var multiply = num1 * num2;
+    console.log("the result", multiply)
+}
+function divideTwoNumbers(num1,num2){
+    var divide = num1 / num2;
+    console.log("the result", divide)
+}
+function subtractTwoNumbers(num1,num2){
+    var substract = num1 - num2;
+    console.log("the result", substract)
+}
+var num1 = readlineSync.questionInt('May I have your first number? ');
+console.log(num1);
+
+var num2 = readlineSync.questionInt("May i have your second number? ");
+console.log(num2)
+
+var chooseWhatYouWant = readlineSync.question("what would you like to do here?/ add, sub, mult, div? ")
+if(chooseWhatYouWant === "add"){
+    addTwoNumbers(num1,num2)
+}else if(chooseWhatYouWant === "mult"){
+    timesTwoNumbers(num1,num2)
+}else if(chooseWhatYouWant === "div"){
+    divideTwoNumbers(num1,num2)
+}else(chooseWhatYouWant === "sub")
+    subtractTwoNumbers(num1,num2)
+
